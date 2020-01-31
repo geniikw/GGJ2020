e1097c5833848e9190ebc3716dfe4d810b618e87
geniikw <geniikw@gmail.com>
2020-02-01 02:49:11 +0900

    접속성공함

--- Assets/NetworkHub.cs
+++ Assets/NetworkHub.cs
@@ -2,33 +2,110 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
-public class NetworkHub : MonoBehaviour
+public class NetworkHub : MonoBehaviourPunCallbacks
 {
     public string gameversion = "1";
+    public InputField nick;
+    public InputField roomNumber;
+    public Text status;
 
-    
-    void Awake(){
+    private bool isJoin = false;
 
-        PhotonNetwork.AutomaticallySyncScene = true;
-    }
-
-    void Start()
+    public override void OnConnectedToMaster()
     {
-        Connect();
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        
+        if (isJoin)
+        {
+            status.text = "방으로 들어갑니다";
+        status.color = Color.white;
+
+            PhotonNetwork.JoinRoom(roomNumber.text);
+        }
+        else
+        {
+            status.text = "방을 만듭니다.";
+        status.color = Color.white;
+
+            PhotonNetwork.CreateRoom(Random.Range(1000, 10000).ToString(), new RoomOptions()
+            {
+                MaxPlayers = 4
+            });
+        }
+
+
     }
 
+    public override void OnJoinedLobby()
+    {
+        status.text = "로비에 들어왔습니다.";
+        status.color = Color.white;
+        PhotonNetwork.JoinRoom(roomNumber.text);
+    }
 
-    void Connect(){
-        if(PhotonNetwork.IsConnected){
+    public override void OnJoinedRoom()
+    {
+        status.text = PhotonNetwork.CurrentRoom.Name + "번 방으로 들어갑니다.";
+        status.color = Color.white;
+
+        Debug.Log("PUN Basics Tutorial/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        Debug.Log(PhotonNetwork.CurrentRoom.Name);
+    }
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        //PhotonNetwork.autoJoinLobby  = true;
+        nick.text = "jammer" + Random.Range(1000, 10000);
+    }
+
+    public void MakeRoom()
+    {
+        PhotonNetwork.NickName = nick.text;
+        isJoin = false;
+        if (PhotonNetwork.IsConnected)
+        {
             PhotonNetwork.JoinRandomRoom();
-                        Debug.Log("b");
         }
-        else{
-            Debug.Log("a");
+        else
+        {
+
             PhotonNetwork.GameVersion = gameversion;
             PhotonNetwork.ConnectUsingSettings();
-        }   
+        }
+    }
+
+    public void JoinRoom()
+    {
+        if (string.IsNullOrEmpty(roomNumber.text))
+        {
+            status.text = "방번호를 입력해야 합니다.";
+            status.color = Color.red;
+            return;
+        }
+
+
+        isJoin = true;
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.NickName = nick.text;
+            PhotonNetwork.JoinRoom(roomNumber.text);
+        }
+        else
+        {
+            PhotonNetwork.GameVersion = gameversion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+
+    }
+
+    public void EnterScene()
+    {
+
     }
 
 }
--- Assets/RoomNetwork.cs
+++ Assets/RoomNetwork.cs
@@ -4,51 +4,7 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 
-public class RoomNetwork : MonoBehaviour, IConnectionCallbacks
+public class RoomNetwork : MonoBehaviourPunCallbacks
 {
-    public void OnConnected()
-    {
-        Debug.Log("Connectted");
-    }
-
-    public void OnConnectedToMaster()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnCustomAuthenticationFailed(string debugMessage)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnCustomAuthenticationResponse(Dictionary<string, object> data)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnDisconnected(DisconnectCause cause)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnRegionListReceived(RegionHandler regionHandler)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    private void OnEnable() {
-        PhotonNetwork.AddCallbackTarget(this);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
