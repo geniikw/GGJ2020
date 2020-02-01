21717ea148564131cf77ff212e7b083425d8ea51
geniikw <geniikw@gmail.com>
2020-02-01 22:40:02 +0900

    some

--- Assets/GGJ2020/Scripts/NetworkHub.cs
+++ Assets/GGJ2020/Scripts/NetworkHub.cs
@@ -96,7 +96,7 @@ public class NetworkHub : MonoBehaviourPunCallbacks
         isJoin = true;
         if (PhotonNetwork.IsConnected)
         {
-            PhotonNetwork.JoinRoom(roomNumber.text);
+            PhotonNetwork.JoinRandomRoom();
         }
         else
         {
